4e0e7bade85c8bbfaa2f6a4a3369ce6c42d945b6
bakito <github@bakito.ch>
2023-12-21 20:47:27 +0100

    update

--- vars/connectToOpenshiftProject.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/groovy
-import com.six_group.jenkins.OpenShiftUtils
-
-def call(Map config, Closure body) {
-    def utils = new OpenShiftUtils()
-    utils.connectToOpenshiftProject(this, config.cluster, config.project, config.credentialsId, body)
-}
--- vars/getCredentialsWithNamespace.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/groovy
-
-import com.six_group.jenkins.OpenShiftUtils
-
-def call(String secretName) {
-    def utils = new OpenShiftUtils()
-    return "${utils.getNamespace()}-${secretName}"
-}
